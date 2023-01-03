15c621672fae708f5d3234c4ffbed26653d06d63
AndresCalvoG <andrescalvo9407@gmail.com>
2023-01-03 09:06:53 -0800

    end class 5 y 6 interpolacion de datos

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const vm = Vue.createApp({
   data() {
     return {
-      text: "Hola Vue",
+      text: "Hola Vue data",
+      title: "<h1>Soy un titulo</h1>",
     };
   },
+  template: `<p>{{text}}</p>
+            <p v-once v-text="text"></p>
+            <div v-html="title"></div>`,
 }).mount("#app");
 
 console.log(vm);
